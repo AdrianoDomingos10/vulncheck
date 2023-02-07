e475845de48da7f2423dd37fc02c157d5c65d1be
Adriano Domingos <67689482+AdrianoDomingos10@users.noreply.github.com>
2023-02-07 19:47:19 +0100

    Add files via upload

---
+++ vulncheck.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Check for vulnerabilities in installed packages
+
+printf "Checking for vulnerabilities in installed packages...\n"
+
+# Use the command line tool 'apt-get' to check for updates
+updates=$(apt-get upgrade --dry-run)
+
+# Check if there are any updates available
+if [[ -n "$updates" ]]; then
+  printf "Vulnerabilities found! Please update the following packages:\n"
+  printf "$updates\n"
+else
+  printf "No vulnerabilities found.\n"
+fi
